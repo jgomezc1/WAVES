17c399d9e0ff533d47705b272bc0ee9933a65307
jgomezc1 <jgomezc1@eafit.edu.co>
2017-11-08 05:21:53 -0500

    Updated folder structure

--- MODELS/MESHER/parameters.py
+++ MODELS/MESHER/parameters.py
@@ -4,14 +4,32 @@ square domain under a point load.
 @autor Juan Gomez
 """
 from __future__ import division
-beta_min  = 300.0
-alpha_max = 600.0
+
+TM = 2.0
+beta_min  =1000.0
+alpha_max =2000.0
 f_max = 4.0
 h_c = beta_min/10.0/f_max
 c_l = 2.0* h_c
 dt = c_l/2.0/20.0/alpha_max
-print h_c , c_l , dt
+nincs = TM/dt
 #
-C_L = 10.0
+print 'DATOS CALCULADOS'
+print '------------------'
+print
+print 'Tamanio caracteristico' , h_c
+print 'Parametro de la malla'  , c_l
+print 'Delta de tiempo' , dt
+print 'Numero de incrementos' , nincs
+#
+print
+print 'DATOS RECALCULADOS'
+print '------------------'
+print
+C_L = 25.0
+H_C = C_L/2.0
 DT = C_L/2.0/20.0/alpha_max
-print DT
+print 'Delta de tiempo recalculado' , DT , 'para nuevo cl'
+print 'Tamanio caracteristico h_c recalculado para nuevo cl' , H_C
+NINCS = TM/DT
+print 'Numero de incrementos' , NINCS , 'para tiempo maximo' , TM
